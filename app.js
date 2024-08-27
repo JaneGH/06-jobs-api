ed96df198eccda2f00a695586427b73fa0691da0
JaneGH <jkrytsyna@gmail.com>
2024-08-27 11:07:23 -0700

    added e-mail sending

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 require('express-async-errors');
 
-// extra security packages
+// Extra security packages
 const helmet = require('helmet');
 const cors = require('cors');
 const xss = require('xss-clean');
@@ -13,6 +13,7 @@ const app = express();
 const connectDB = require('./db/connect');
 const authenticateUser = require('./middleware/authentication');
 
+// Middleware setup
 app.set('trust proxy', 1);
 app.use(
   rateLimiter({
@@ -21,30 +22,28 @@ app.use(
   })
 );
 
-app.use(express.json());
+app.use(express.json()); // Only one instance needed
 app.use(helmet());
 app.use(cors());
 app.use(xss());
 
-app.use(express.json());
-
-app.get('/', (req, res) => {
-  res.send('Jobs API');
+app.get('/api/v1/', (req, res) => {
+  res.send('Api goals');
 });
 
-// routers
+// Routes setup
 const authRouter = require('./routes/auth');
 const goalsRouter = require('./routes/goals');
+const emailRoutes = require('./routes/email');
 
-// error handler
-const notFoundMiddleware = require('./middleware/not-found');
-const errorHandlerMiddleware = require('./middleware/error-handler');
-
-app.use(express.json());
-
-// routes
+// Verify routes
+app.use('/api/v1/email', authenticateUser, emailRoutes);
 app.use('/api/v1/auth', authRouter);
 app.use('/api/v1/goals', authenticateUser, goalsRouter);
+
+// Error handlers
+const notFoundMiddleware = require('./middleware/not-found');
+const errorHandlerMiddleware = require('./middleware/error-handler');
 
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
